7a39ce3dd95ec161b6652626bc00f17e2c807ba8
Anmolj0815 <122198912+Anmolj0815@users.noreply.github.com>
2025-09-03 01:03:01 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Explicitly set root to current directory
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
 });
